d6588e408ec8bd5fde272c4bfb3ad9e570f212bf
Abhishek Yelisetti <abhishek.yelisetti@zemosolabs.com>
2021-09-20 11:05:43 +0530

    Added evaluate header conditions

--- src/main/java/com/example/demo/FileValidations.java
+++ src/main/java/com/example/demo/FileValidations.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Scanner;
 import java.util.zip.ZipEntry;
@@ -81,22 +82,51 @@ public class FileValidations {
 		return true;
 	}
 	
-	private static void evaluateHeaders(InputStream stream) {
+	private static boolean evaluateHeaders(InputStream stream) {
         
         Scanner scanner = new Scanner(stream).useDelimiter(",");
         
-//        private final String mandatoryHeaders[] = new String[] {
-//        		
-//        }
-        
+        ArrayList<String> foundHeadersList = new ArrayList<>();
+        ArrayList<String> remainingHeadersList = getMandatoryHeaders();
         
         while(scanner.hasNext()) {
         	String header = scanner.next();
         	if(!isHeader(header)) {
         		break;
         	}
+        	else {
+        		if(header.startsWith("ct") || header.startsWith("aq")) {
+        			if(header.endsWith("_o") || header.endsWith("_m")) {
+        				if(remainingHeadersList.contains(header) && !foundHeadersList.contains(header)) {
+        					remainingHeadersList.remove(header);
+        					foundHeadersList.add(header);
+        				}
+        				else if(foundHeadersList.contains(header) || !remainingHeadersList.contains(header)) {
+        					return false;
+        				}
+        			}
+        		}
+        		else if(header.startsWith("cp")) {
+        			
+        		}
+        	}
         }
-        
+        return true;
+	}
+
+	private static ArrayList<String> getMandatoryHeaders() {
+		return new ArrayList<String>() {
+			{
+				add("ct_type_m");
+				add("ct_label_m");
+				add("ct_empty_mass_mg_o");
+				add("ct_suggested_barcode_o");
+				add("aq_well_index_m");
+				add("aq_well_label_o");
+				add("aq_vol_ul_o");
+				add("aq_mass_mg_o");
+			}
+		};
 	}
 
 	private static boolean isHeader(String header) {
